fc57c51f4eda6024e6053f2a831004d9fbdf3743
Inf1nity <fvendetta311@gmail.com>
2025-11-29 10:37:53 +0300

    fix

--- utils/browser_assisnant.py
+++ utils/browser_assisnant.py
@@ -16,71 +16,70 @@ class BrowserAssistant:
     def start(self):
         self.browser_controller.start_browser()
         model = None
-
-        try:
+        while 1:
+            if model and model.status == "waiting_user_input":
+                for i in model.missing_data:
+                    print(i.question)
+            user_input = input("(enter `q` to exit)>>> ")
+            if user_input.lower() == 'q':
+                self.browser_controller.close_browser()
+                break
             while 1:
-                if model and model.status == "waiting_user_input":
-                    for i in model.missing_data:
-                        print(i.question)
-                user_input = input(">>> ")
-                while 1:
-                    if user_input:
-                        response = self.assistant.chat(user_input)
-                        user_input = None
-                    else:
-                        response = self.assistant.chat(msg, role="system")
-                    msg = ''
+                if user_input:
+                    response = self.assistant.chat(user_input)
+                    user_input = None
+                else:
+                    response = self.assistant.chat(msg, role="system")
+                msg = ''
 
-                    try:
-                        model = models.AssistantResponse.model_validate_json(response)
-                    except Exception as e:
-                        msg = f"Error parsing assistant response: {e}"
-                        continue
+                try:
+                    model = models.AssistantResponse.model_validate_json(response)
+                except Exception as e:
+                    msg = f"Error parsing assistant response: {e}"
+                    continue
 
-                    try:
+                try:
 
-                        for action in model.action_sequence:
-                            print(f'func - {action.function} | reason - {action.reason}')
-                            if action.function == "open":
-                                self.browser_controller.open(**action.args)
-                                msg += self.browser_controller.get_html()
-                                continue
-                            elif action.function == "click":
-                                error = self.browser_controller.click_element(**action.args)
-                                if error:
-                                    msg += f"Error during clicking element: {error}"
-                                    break
-                                msg += self.browser_controller.get_html()
-                                continue
-                            elif action.function == "enter":
-                                error = self.browser_controller.enter(**action.args)
-                                if error:
-                                    msg += f"Error during entering text: {error}"
-                                    break
-                                msg += self.browser_controller.get_html()
-                                continue
-                            elif action.function == "get":
-                                msg += self.browser_controller.get_html()
-                                continue
-                            elif action.function == "wait":
-                                self.browser_controller.wait(**action.args)
-                                msg += self.browser_controller.get_html()
-                                continue
-                            elif action.function == "save_response":
-                                self.assistant.save_response(**action.args)
-                                continue
-                            elif action.function == "delete_response":
-                                self.assistant.delete_response(**action.args)
-                                continue
-                            else:
-                                msg += "Unknown action:" + str(action.function)
+                    for action in model.action_sequence:
+                        print(f'func - {action.function} | reason - {action.reason}')
+                        if action.function == "open":
+                            self.browser_controller.open(**action.args)
+                            msg += self.browser_controller.get_html()
+                            continue
+                        elif action.function == "click":
+                            error = self.browser_controller.click_element(**action.args)
+                            if error:
+                                msg += f"Error during clicking element: {error}"
                                 break
-                            
-                        if model.status != "in_progress":
+                            msg += self.browser_controller.get_html()
+                            continue
+                        elif action.function == "enter":
+                            error = self.browser_controller.enter(**action.args)
+                            if error:
+                                msg += f"Error during entering text: {error}"
+                                break
+                            msg += self.browser_controller.get_html()
+                            continue
+                        elif action.function == "get":
+                            msg += self.browser_controller.get_html()
+                            continue
+                        elif action.function == "wait":
+                            self.browser_controller.wait(**action.args)
+                            msg += self.browser_controller.get_html()
+                            continue
+                        elif action.function == "save_response":
+                            self.assistant.save_response(**action.args)
+                            continue
+                        elif action.function == "delete_response":
+                            self.assistant.delete_response(**action.args)
+                            continue
+                        else:
+                            msg += "Unknown action:" + str(action.function)
                             break
+                        
+                    if model.status != "in_progress":
+                        break
 
-                    except Exception as e:
-                        msg += f"Error during action execution: {e}"
-                        continue
-        finally:
-            self.browser_controller.driver.quit()
+                except Exception as e:
+                    msg += f"Error during action execution: {e}"
+                    continue
